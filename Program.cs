0d56219a554c23ffd41c5506060ef2045803bf98
ALEX K. KEMBOI <AKEMBOI@fintech-group.com>
2023-06-22 17:28:49 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        StudentsScores studentsScores = new StudentsScores();
+        studentsScores.Main();
+    }
+}
+class StudentsScores
+{
+    public List<int> marks;
+    public  StudentsScores()
+    {
+        marks = new List<int>();
+    }
+    public void AddMarks()
+    {
+        Console.WriteLine("Enter the number of students you want to record marks for");
+        int noOfStudents=int.Parse(Console.ReadLine());
+        while (marks.Count <= noOfStudents) {
+            Console.WriteLine($"Enter marks scored by student {marks.Count}");
+            int newMark = int.Parse(Console.ReadLine());
+            marks.Add(newMark);
+            if(marks.Count==noOfStudents)
+            {
+                Console.WriteLine($"All {marks.Count} entered succefully");
+                StudentsScores studentsScores = new StudentsScores();
+                studentsScores.Main();
+            }
+        }
+    }
+
+    public void ViewMarks()
+    {
+        for (int i = 0; i < marks.Count; i++)
+        {
+            Console.WriteLine($"Student {i + 1}: {marks[i]}");
+        }
+    }
+
+    public void RemoveMarks()
+    {
+        int index = int.Parse(Console.ReadLine());
+        marks.RemoveAt(index);
+    }
+   
+
+
+    public void ExitApp()
+    {
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+    }
+    public void Main()
+    {
+        StudentsScores studentsScores = new StudentsScores();
+        Console.WriteLine("Welcome to students marks analysis");
+        Console.WriteLine("Choose an option");
+        Console.WriteLine("1:Add Marks");
+        Console.WriteLine("2:View analysis");
+        Console.WriteLine("3:Exit app");
+        int option = int.Parse(Console.ReadLine());
+        switch (option)
+        {
+            case 1:
+                studentsScores.AddMarks();
+                break;
+            case 2:
+                studentsScores.ViewMarks();
+                break;
+            case 3:
+                studentsScores.ExitApp();
+                break;
+            default:
+                Console.WriteLine("Wrong entry try again");
+                break;
+        }
+    }
+}
